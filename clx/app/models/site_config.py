5e81aa3a7284555e2d4290e388d379544679c9b5
Nathan Dahlberg <nadahlberg@gmail.com>
2026-09-17 00:22:41 -0400

    fix(site-config): default the site name to CLX

--- clx/app/models/site_config.py
+++ clx/app/models/site_config.py
@@ -11,7 +11,7 @@ class SiteConfig(BaseModel):
     id = models.UUIDField(
         primary_key=True, default=SITE_CONFIG_ID, editable=False
     )
-    site_name = models.CharField(max_length=100, default="clx")
+    site_name = models.CharField(max_length=100, default="CLX")
 
     class Meta:
         constraints = [
